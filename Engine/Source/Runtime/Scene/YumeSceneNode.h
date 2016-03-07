1f6f0c5070298795f4b737b2c5382037a40baedc
arkenthera <arkengraphics@gmail.com>
2016-03-07 22:46:15 +0200

    Fix typo,msvc pls warn me

--- Engine/Source/Runtime/Scene/YumeSceneNode.h
+++ Engine/Source/Runtime/Scene/YumeSceneNode.h
@@ -396,7 +396,7 @@ namespace YumeEngine
 
 		if(recursive)
 		{
-			for(YumeVector<SharedPtr<YumeSceneNode> >::const_iterator i = children_.Begin(); i != children_.end(); ++i)
+			for(YumeVector<SharedPtr<YumeSceneNode> >::const_iterator i = children_.begin(); i != children_.end(); ++i)
 			{
 				T* component = (*i)->GetDerivedComponent<T>(true);
 				if(component)
