efa1c0d7c6d569d34698b2eaeb9ec7fa9ae15519
arkenthera <arkengraphics@gmail.com>
2016-03-12 02:42:47 +0200

    Will this compile?

---
+++ Samples/HelloWorld/Cube.h
@@ -0,0 +1,51 @@
+//----------------------------------------------------------------------------
+//Yume Engine
+//Copyright (C) 2015  arkenthera
+//This program is free software; you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation; either version 2 of the License, or
+//(at your option) any later version.
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//You should have received a copy of the GNU General Public License along
+//with this program; if not, write to the Free Software Foundation, Inc.,
+//51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.*/
+//----------------------------------------------------------------------------
+//
+// File : <Filename>
+// Date : <Date>
+// Comments :
+//
+//----------------------------------------------------------------------------
+#ifndef __YumeCube_h__
+#define __YumeCube_h__
+//----------------------------------------------------------------------------
+#include "YumeRequired.h"
+#include "Renderer/YumeDrawable.h"
+//----------------------------------------------------------------------------
+namespace YumeEngine
+{
+	class YumeGeometry;
+	class Cube : public YumeDrawable
+	{
+	public:
+		Cube();
+		virtual ~Cube();
+
+		static YumeHash GetTypeStatic() { return type_; };
+		virtual YumeHash GetType() { return type_; };
+		static YumeHash type_;
+
+		virtual void OnWorldBoundingBoxUpdate();
+		virtual void UpdateBatches(const FrameInfo& frame);
+
+		SharedPtr<YumeGeometry> geo_;
+
+	};
+}
+
+
+//----------------------------------------------------------------------------
+#endif
